87618b0fe56319a9b98cfa68abe84dfa2ed32ec7
Tommy <ernsund@gmail.com>
2019-06-27 18:44:19 +0200

    Updates

---
+++ precache-manifest.d1b3cecfeb06f6f0dcd9ca6e3d8d2895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97673e5b75c46f45523fe24932d18125",
+    "url": "/squares-page/index.html"
+  },
+  {
+    "revision": "208e4a75d1759c38b754",
+    "url": "/squares-page/static/css/main.534a2415.chunk.css"
+  },
+  {
+    "revision": "fe71f8cb2c00167c5e2f",
+    "url": "/squares-page/static/js/2.3f0ca834.chunk.js"
+  },
+  {
+    "revision": "208e4a75d1759c38b754",
+    "url": "/squares-page/static/js/main.36db5a85.chunk.js"
+  },
+  {
+    "revision": "cd052f7c0c40ba19dacb",
+    "url": "/squares-page/static/js/runtime~main.0e4b58d4.js"
+  }
+]);
